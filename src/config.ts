427d28e2ba7e6d8ee3e3eec8523dec84a47c0499
krish <hi@xkrish.co>
2024-03-03 14:08:07 +0530

    Super Commit

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import type { NavigationItem } from "@/types/navigation"
-import { Twitter, Github, type LucideIcon } from 'lucide-react'
+import { type LucideIcon, Twitter, Github, Linkedin, Mail, Instagram, Package, Rocket } from 'lucide-react'
+import type { ButtonProps } from "./components/ui/button"
 
 const config: Config = {
   name: 'Krish Gupta',
@@ -25,6 +26,105 @@ const config: Config = {
       icon: Github,
       name: 'GitHub',
       url: 'https://github.com/xkrishguptaa'
+    },
+    {
+      icon: Linkedin,
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/xkrishguptaa'
+    },
+    {
+      icon: Instagram,
+      name: 'Instagram',
+      url: 'https://www.instagram.com/xkrishguptaa'
+    },
+    {
+      icon: Mail,
+      name: 'Email',
+      url: 'mailto:hi@xkrish.co'
+    }
+  ],
+  projects: [
+    {
+      name: 'Reseter.css',
+      description: 'The modern CSS reset (3m+ JSDelivr Hits, 70k+ NPM Installs, 1.4k+ stars)',
+      buttons: [
+        {
+          url: 'https://npmjs.com/package/reseter.css',
+          icon: Package,
+          text: 'NPM Install',
+          variant: 'default',
+          size: 'default'
+        },
+        {
+          url: 'https://www.jsdelivr.com/package/npm/reseter.css',
+          icon: Rocket,
+          text: 'Use CDN',
+          variant: 'secondary',
+          size: 'default'
+        },
+        {
+          url: 'https://github.com/xkrishguptaa/reseter.css',
+          icon: Github,
+          size: 'icon'
+        }
+      ],
+      showcase: true
+    },
+    {
+      name: 'Repo Rater Bot',
+      description: 'Discord Bot that wraps around the EddieHub Repo Rater API for easier access',
+      buttons: [
+        {
+          url: 'https://github.com/xkrishguptaa/repo-rater-bot',
+          icon: Github,
+          text: 'View on GitHub',
+          variant: 'default',
+          size: 'default'
+        }
+      ],
+      showcase: true
+    },
+    {
+      name: 'BioDrop Self',
+      description: 'Concept Application for self-hosted UI for BioDrop',
+      buttons: [
+        {
+          url: 'https://biodrop-self.xkrish.co',
+          icon: Rocket,
+          text: 'View Live',
+          variant: 'default',
+          size: 'default'
+        },
+        {
+          url: 'https://github.com/xkrishguptaa/biodrop-self',
+          icon: Github,
+          text: 'View on GitHub',
+          variant: 'secondary',
+          size: 'default'
+        }
+      ],
+      showcase: true
+    },
+    {
+      name: 'Azelf',
+      description: 'Download, Optimize and Save Images from the Web',
+      buttons: [
+        {
+          url: 'https://npmjs.com/package/azelf',
+          icon: Package,
+          text: 'NPM Install',
+          variant: 'default',
+          size: 'default'
+        },
+        {
+          url: 'https://github.com/xkrishguptaa/azelf',
+          icon: Github,
+          text: 'View on GitHub',
+          variant: 'secondary',
+          size: 'default'
+        }
+      ],
+      showcase: true
     }
   ]
 }
@@ -38,7 +138,23 @@ interface Config {
     name: string
     icon: LucideIcon
     url: string
-  }[]
+  }[],
+  projects: Array <{
+    name: string
+    description: string
+    buttons: Array<{
+      text: string
+      url: string
+      icon: LucideIcon
+      variant: ButtonProps['variant']
+      size: Exclude<ButtonProps['size'], 'icon'>
+    } | {
+      url: string
+      icon: LucideIcon
+      size: 'icon'
+    }>
+    showcase: boolean
+  }>
 }
 
 export default config
